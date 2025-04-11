b74679c83619df5184f3dced35295eef12d4095c
SangJee Park <sjpark3927@gmail.com>
2025-04-12 02:00:45 +0900

    first

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../utils/api";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [user, setUser] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await api.post("/user/login", { email, password });
+        // 로그인 성공      
+      if (response.status === 200) {
+        setUser(response.data.user)
+        //세션 스토리지에 토큰 값 저장하기 
+        sessionStorage.setItem("token", response.data.token);
+        //토큰값 api 보내주기
+        api.defaults.headers["authorization"] = "Bearer "+response.data.token
+        setError("");
+        navigate("/");
+        
+      }
+      throw new Error(response.message);
+    } catch (error) {
+      // 서버 응답 에러 메시지 처리
+      setError(error.message);
+    }
+  };  
+
   return (
     <div className="display-center">
-      <Form className="login-box">
+      {error && <div className="red-error">{error}</div>}
+      <Form className="login-box" onSubmit = {handleLogin}>
         <h1>로그인</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={(event)=>setEmail(event.target.value)}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(event)=>setPassword(event.target.value)}/>
         </Form.Group>
         <div className="button-box">
           <Button type="submit" className="button-primary">
